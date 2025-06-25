0fed86be4683c238e461770f50132232b3a38c4e
Muhammad Alif <mhdalif.id@gmail.com>
2025-06-25 13:07:50 +0800

    Fix Auth Routes

--- backend/src/auth/auth.routes.js
+++ backend/src/auth/auth.routes.js
@@ -1,13 +1,18 @@
 import express from 'express';
-import { login, register, verifyEmail, requestPasswordReset, resetPassword } from './auth.controller.js'; 
+import { 
+    register, 
+    login, 
+    verifyEmail,
+    requestPasswordReset,
+    resetPassword
+} from './auth.controller.js';
 
 const router = express.Router();
 
-router.post('/login', login);
 router.post('/register', register);
-router.get('/verify-email/:token', verifyEmail); 
+router.post('/login', login); // PERBAIKAN DI SINI: diubah dari .get menjadi .post
+router.get('/verify-email/:token', verifyEmail);
 router.post('/request-password-reset', requestPasswordReset);
 router.post('/reset-password/:token', resetPassword);
-
 
 export default router;
