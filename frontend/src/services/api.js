3eb011f3556515ac1838113ecf9bfb16b4f813e4
Muhammad Alif <mhdalif.id@gmail.com>
2025-06-26 08:23:13 +0800

    Fix API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+  // Langsung arahkan ke alamat API produksi Anda
+  baseURL: 'https://api.stockwatch.web.id/api'
 });
 
 api.interceptors.request.use(config => {
